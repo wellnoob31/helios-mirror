38c7c8acc77c5dc36a0dae844f98ad4045de7d67
wellnoob31 <107308485+wellnoob31@users.noreply.github.com>
2022-06-27 20:32:50 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("go away")
-    buttons.buildbutton("sed")
+   
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
